84644004a50bc8b0715039491ac8c25dadf97ec5
James Sullivan <jsullivan@cloudanswers.com>
2021-06-08 18:42:51 +0200

    enable component for use in communities

--- force-app/main/default/lwc/videoViewer/utils.js
+++ force-app/main/default/lwc/videoViewer/utils.js
@@ -8,15 +8,13 @@
 /**
  * Util methods
  */
-const CUSTOM_PART = "--c";
 const DOWNLOAD_PART =
-  ".documentforce.com/sfc/servlet.shepherd/document/download/";
+  "/sfc/servlet.shepherd/document/download/";
 
 // Returns either an empty string or the org's base video download url
 export function getBaseVideoUrl(baseUrl) {
   if (baseUrl) {
-    const domain = baseUrl.split(".")[0];
-    return `${domain}${CUSTOM_PART}${DOWNLOAD_PART}`;
+    return `${baseUrl}${DOWNLOAD_PART}`;
   }
   return "";
 }
